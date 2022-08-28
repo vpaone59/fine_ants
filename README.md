f17257bd1018ee5f498af61f6d655aa785ef9b82
Vincent Paone <55220731+vpaone59@users.noreply.github.com>
2022-08-27 23:06:56 -0400

    fixed formatting and cleaned code
    reformatted credit and debit functions
    shortened head tail function, should probably delete this as it's useless!!!!!!!!!!
    also updated readme

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # fine_ants
+
+Provide a CSV file with the phrase "transactions" in the filename in the same directory as this file. It is preferred you use a file just named "transactions.csv", with a column for debit, credit and date.
+
+Example output below: 
+>>>
+fine_ants.py...working...
+
+C:\Users\Vincent\Documents\VSCODE\fine_ants\dfadfasada stransactions (2).csv
+reading C:\Users\Vincent\Documents\VSCODE\fine_ants\dfadfasada stransactions (2).csv...
+
+VIEWING TRANSACTION DATE RANGE: 2022-06-06 ---TO--- 2022-08-26
+Oldest Transaction Date--------- 2022-06-06
+Last Transaction Date----------- 2022-08-26
+**Debit:
+most recent: $3.0       2022-08-26
+oldest: $5.31   2022-06-06
+
+0 167
+**Credit:
+most recent: $1861.33   2022-08-19
+oldest: $1834.73        2022-06-10
+
+Total Debit Transactions from 2022-06-06 to 2022-08-26 = $13112.98
+Total Credit Transactions from 2022-06-06 to 2022-08-26 = $13173.7
+<<<
--- fine_ants.py
+++ fine_ants.py
@@ -1,59 +1,111 @@
 # expense tracking
-
-import time
-import csv
 import os
 # pandas >>>>>>> csv
 import pandas as pd
 import numpy as np
+import math
+
+# gets the range of dates (oldest and most recent) and prints them out
 
 
 def get_date_head_tail(data):
-    print("get_date_head_tail...working...")
 
-    earliest = data.head(1).values
-    latest = data.tail(1).values
+    oldest = data[len(data)-1]
+    latest = data[0]
     print(
-        f"VIEWING TRANSACTION DATE RANGE: {latest} ---TO--- {earliest}")
+        f"VIEWING TRANSACTION DATE RANGE: {oldest} ---TO--- {latest}")
+    return oldest, latest
 
 
 def export(data):
     print("p")
 
 
+# gets the first and last debit item from the debit column and then finds the total
 def add_debit(data):
-    print("\nadd_debit...working...")
+    recent_deb = data["Debit"][0]
+    oldest_deb = data["Debit"][len(data)-1]
+    recent_deb_date = data["Date"][0]
+    oldest_deb_date = data["Date"][len(data)-1]
 
-    debit_date_range = data["Date"]
-    get_date_head_tail(debit_date_range)
+    while math.isnan(recent_deb):
+        rc = rc + 1
+        recent_deb = data["Debit"][rc]
+
+    while math.isnan(oldest_deb):
+        oc = oc - 1
+        oldest_deb = data["Debit"][oc]
+
     print(
-        f"\n**Debit most recent: \n{data.head(1)}\noldest: \n{data.tail(1)}\n\n")
+        f"**Debit:\nmost recent: ${recent_deb}\t{recent_deb_date}\noldest: ${oldest_deb}\t{oldest_deb_date}\n")
     debit_trans = data["Debit"]
     deb_total = debit_trans.sum()
     return round(deb_total, 2)
 
 
+# gets the first and last credit item from the credit column and then finds the total
 def add_credit(data):
-    print("\nadd_credit...working...")
+    rc = 0
+    oc = len(data)-1
+    print(rc, oc)
+    recent_cre = data["Credit"][rc]
+    oldest_cre = data["Credit"][oc]
 
-    credit_date_range = data["Date"]
-    get_date_head_tail(credit_date_range)
+    while math.isnan(recent_cre):
+        rc = rc + 1
+        recent_cre = data["Credit"][rc]
+
+    while math.isnan(oldest_cre):
+        oc = oc - 1
+        oldest_cre = data["Credit"][oc]
+
+    recent_cre_date = data["Date"][rc]
+    oldest_cre_date = data["Date"][oc]
+
     print(
-        f"\n**Credit most recent: \n{data.head(1)}\noldest: \n{data.tail(1)}\n\n")
+        f"**Credit:\nmost recent: ${recent_cre}\t{recent_cre_date}\noldest: ${oldest_cre}\t{oldest_cre_date}\n")
     credit_trans = data["Credit"]
     cre_total = credit_trans.sum()
-    return (round(cre_total, 2))
+    return round(cre_total, 2)
 
 
 def main():
     print('\nfine_ants.py...working...\n')
-    myfile = pd.read_csv(os.getcwd() + "/fine_ants/transactions.csv")
+    directory = os.getcwd()
+    for filename in os.listdir(directory):
+        f = os.path.join(directory, filename)
+        if os.path.isfile(f):
+            print(f)
+            # as long as the file is a CSV that contains the string "transactions" then it should work
+            if f.endswith(".csv"):
+                if "transactions" in f:
+                    file = f
+                    break
+                else:
+                    print(
+                        f"CSV file {f} does not contain phrase \"transactions\".\nRecommended to name file 'transactions.csv")
+            else:
+                print("File is not of type CSV")
+
+    # read the CSV file
+    print(f"reading {file}...\n")
+    myfile = pd.read_csv(file)
+
+    # use only the date and debit/credit columns
     focus_deb = myfile[["Date", "Debit"]]
     focus_cre = myfile[["Date", "Credit"]]
 
-    add_debit(focus_deb)
+    # get the last and most recent date in the date column
+    oldest, latest = get_date_head_tail(myfile["Date"])
+    print(f"Oldest Transaction Date--------- {oldest}")
+    print(f"Last Transaction Date----------- {latest}")
 
-    add_credit(focus_cre)
+    total_debit = add_debit(focus_deb)
+    total_credit = add_credit(focus_cre)
+    print(
+        f"Total Debit Transactions from {oldest} to {latest} = ${total_debit}")
+    print(
+        f"Total Credit Transactions from {oldest} to {latest} = ${total_credit}")
 
 
 if __name__ == '__main__':
